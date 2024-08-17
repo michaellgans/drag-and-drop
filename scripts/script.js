1b20657fdf27849f450896ae51db893d9ae0210d
michaellgans <michaellgans@Victoria>
2024-08-17 14:49:05 -0500

    MLG - 2:48 PM - finishes drag and drop mechanic

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,25 @@
 // JavaScript to Apply Drag and Drop
 
 $(document).ready(function() {
-    const pieces = $(".piece");
-    const boardSpace1 = $("#1");
 
-    for (piece of pieces) {
-        $(".piece").on("dragstart", function(event) {
-            let selectedPiece = event.target;
+    let selectedPiece = null;
 
-            $(".board-space").on("dragover", function(event) {
-                event.preventDefault();
-            });
+    $(".piece").on("dragstart", function(event) {
+        selectedPiece = event.target;
 
-            $(".board-space").on("drop", function(event) {
-                boardSpace1.append(selectedPiece);
-                selectedPiece = null;
-            });
+        $(".board-space").on("dragover", function(event) {
+            event.preventDefault();
         });
-    }
+
+        $(".board-space").on("drop", function(event) {
+            event.preventDefault();
+            if (selectedPiece) {
+                let $this = $(this);
+                if ($this.children().length === 0) {
+                    $(this).append(selectedPiece);
+                }
+                selectedPiece = null;
+            }
+        });
+    });
 });
